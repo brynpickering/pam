763eb25e7e59351031c701431bae08f99ed02af2
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-26 11:02:32 +0100

    Tighten seconds

--- tests/test_100_memory_profiling.py
+++ tests/test_100_memory_profiling.py
@@ -6,7 +6,7 @@ import pytest
 from pam import read
 
 BENCHMARK_MEM = "1400 MB"
-BENCHMARK_SECONDS = 300
+BENCHMARK_SECONDS = 250
 
 data_dir = Path(__file__).parent / "test_data"
 
@@ -17,12 +17,6 @@ def trips_attrs():
     attributes = pd.read_csv(data_dir / "extended_persons_data.csv.gz")
     attributes.set_index("pid", inplace=True)
     return trips, attributes
-
-
-@pytest.mark.limit_memory(BENCHMARK_MEM)
-@pytest.mark.high_mem
-def test_activity_loader_mem(trips_attrs):
-    read.load_travel_diary(*trips_attrs)
 
 
 @pytest.mark.timeout(BENCHMARK_SECONDS, func_only=True)
